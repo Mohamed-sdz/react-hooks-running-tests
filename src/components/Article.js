3c903cbb6a538180c7cd232149424387a11fd1f8
saiderrahmani <didi.mousedidi@outlook.com>
2023-05-22 22:06:05 -0400

    update components

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,15 @@
 import React from "react";
-
 function Article() {
   return (
     <div>
+      <body>
+  <div>
+    <div>please pass this test</div>
+  </div>
+</body>
       {/* please pass this test */}
     </div>
   );
 }
-
 export default Article;
+
